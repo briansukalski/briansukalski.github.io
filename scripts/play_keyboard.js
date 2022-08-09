58e9c45df49a5b947fa142dfd863d3aaf7a29a8a
briansukalski <sukalskibrian@gmail.com>
2022-08-08 21:16:25 -0500

    Add rest of audio files for octave, and copy play functions to make them all interactable

--- scripts/play_keyboard.js
+++ scripts/play_keyboard.js
@@ -1,8 +1,81 @@
 let c_1 = document.getElementById('c-natural-1');
+let c_sharp_1 = document.getElementById('c-sharp');
+let d_1 = document.getElementById('d-natural');
+let d_sharp_1 = document.getElementById('d-sharp');
+let e_1 = document.getElementById('e-natural');
+let f_1 = document.getElementById('f-natural');
+let f_sharp_1 = document.getElementById('f-sharp');
+let g_1 = document.getElementById('g-natural');
+let g_sharp_1 = document.getElementById('g-sharp');
+let a_1 = document.getElementById('a-natural');
+let a_sharp_1 = document.getElementById('a-sharp');
+let b_1 = document.getElementById('b-natural');
+let c_2 = document.getElementById('c-natural-2');
 
-const play_note = () => {
+
+const play_c_1 = () => {
     let audio = new Audio('../style/media/audio/keyboard_tones/c_1.mp3');
     audio.play();
 }
+const play_c_sharp_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/c_sharp_1.mp3');
+    audio.play();
+}
+const play_d_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/d_1.mp3');
+    audio.play();
+}
+const play_d_sharp_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/d_sharp_1.mp3');
+    audio.play();
+}
+const play_e_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/e_1.mp3');
+    audio.play();
+}
+const play_f_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/f_1.mp3');
+    audio.play();
+}
+const play_f_sharp_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/f_sharp_1.mp3');
+    audio.play();
+}
+const play_g_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/g_1.mp3');
+    audio.play();
+}
+const play_g_sharp_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/g_sharp_1.mp3');
+    audio.play();
+}
+const play_a_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/a_1.mp3');
+    audio.play();
+}
+const play_a_sharp_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/a_sharp_1.mp3');
+    audio.play();
+}
+const play_b_1 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/b_1.mp3');
+    audio.play();
+}
+const play_c_2 = () => {
+    let audio = new Audio('../style/media/audio/keyboard_tones/c_2.mp3');
+    audio.play();
+}
 
-c_1.addEventListener("mousedown", play_note);
+c_1.addEventListener("mousedown", play_c_1);
+c_sharp_1.addEventListener("mousedown", play_c_sharp_1);
+d_1.addEventListener("mousedown", play_d_1);
+d_sharp_1.addEventListener("mousedown", play_d_sharp_1);
+e_1.addEventListener("mousedown", play_e_1);
+f_1.addEventListener("mousedown", play_f_1);
+f_sharp_1.addEventListener("mousedown", play_f_sharp_1);
+g_1.addEventListener("mousedown", play_g_1);
+g_sharp_1.addEventListener("mousedown", play_g_sharp_1);
+a_1.addEventListener("mousedown", play_a_1);
+a_sharp_1.addEventListener("mousedown", play_a_sharp_1);
+b_1.addEventListener("mousedown", play_b_1);
+c_2.addEventListener("mousedown", play_c_2);
