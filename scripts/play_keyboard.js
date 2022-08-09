dcd4cbcbc538f20a5ddf7f3e0be595467fbd1da1
briansukalski <sukalskibrian@gmail.com>
2022-08-08 21:37:36 -0500

    Simplify js file to run the same function with different input for every key

--- scripts/play_keyboard.js
+++ scripts/play_keyboard.js
@@ -1,81 +1,24 @@
-let c_1 = document.getElementById('c-natural-1');
-let c_sharp_1 = document.getElementById('c-sharp');
-let d_1 = document.getElementById('d-natural');
-let d_sharp_1 = document.getElementById('d-sharp');
-let e_1 = document.getElementById('e-natural');
-let f_1 = document.getElementById('f-natural');
-let f_sharp_1 = document.getElementById('f-sharp');
-let g_1 = document.getElementById('g-natural');
-let g_sharp_1 = document.getElementById('g-sharp');
-let a_1 = document.getElementById('a-natural');
-let a_sharp_1 = document.getElementById('a-sharp');
-let b_1 = document.getElementById('b-natural');
-let c_2 = document.getElementById('c-natural-2');
+let audio_dict = {};
 
+audio_dict['c-natural-1'] = 'c_1';
+audio_dict['c-sharp'] = 'c_sharp_1';
+audio_dict['d-natural'] = 'd_1';
+audio_dict['d-sharp'] = 'd_sharp_1';
+audio_dict['e-natural'] = 'e_1';
+audio_dict['f-natural'] = 'f_1';
+audio_dict['f-sharp'] = 'f_sharp_1';
+audio_dict['g-natural'] = 'g_1';
+audio_dict['g-sharp'] = 'g_sharp_1';
+audio_dict['a-natural'] = 'a_1';
+audio_dict['a-sharp'] = 'a_sharp_1';
+audio_dict['b-natural'] = 'b_1';
+audio_dict['c-natural-2'] = 'c_2';
 
-const play_c_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/c_1.mp3');
-    audio.play();
-}
-const play_c_sharp_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/c_sharp_1.mp3');
-    audio.play();
-}
-const play_d_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/d_1.mp3');
-    audio.play();
-}
-const play_d_sharp_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/d_sharp_1.mp3');
-    audio.play();
-}
-const play_e_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/e_1.mp3');
-    audio.play();
-}
-const play_f_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/f_1.mp3');
-    audio.play();
-}
-const play_f_sharp_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/f_sharp_1.mp3');
-    audio.play();
-}
-const play_g_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/g_1.mp3');
-    audio.play();
-}
-const play_g_sharp_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/g_sharp_1.mp3');
-    audio.play();
-}
-const play_a_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/a_1.mp3');
-    audio.play();
-}
-const play_a_sharp_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/a_sharp_1.mp3');
-    audio.play();
-}
-const play_b_1 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/b_1.mp3');
-    audio.play();
-}
-const play_c_2 = () => {
-    let audio = new Audio('../style/media/audio/keyboard_tones/c_2.mp3');
+function play_note(event) {
+    let audio = new Audio(`../style/media/audio/keyboard_tones/${audio_dict[event.target.id]}.mp3`);
     audio.play();
 }
 
-c_1.addEventListener("mousedown", play_c_1);
-c_sharp_1.addEventListener("mousedown", play_c_sharp_1);
-d_1.addEventListener("mousedown", play_d_1);
-d_sharp_1.addEventListener("mousedown", play_d_sharp_1);
-e_1.addEventListener("mousedown", play_e_1);
-f_1.addEventListener("mousedown", play_f_1);
-f_sharp_1.addEventListener("mousedown", play_f_sharp_1);
-g_1.addEventListener("mousedown", play_g_1);
-g_sharp_1.addEventListener("mousedown", play_g_sharp_1);
-a_1.addEventListener("mousedown", play_a_1);
-a_sharp_1.addEventListener("mousedown", play_a_sharp_1);
-b_1.addEventListener("mousedown", play_b_1);
-c_2.addEventListener("mousedown", play_c_2);
+for (let el in audio_dict) {
+    document.getElementById(el).addEventListener('mousedown', play_note);
+}
