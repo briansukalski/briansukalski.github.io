9466f649d525726f18f53cef760e0292d5a8e562
briansukalski <sukalskibrian@gmail.com>
2022-08-10 22:15:47 -0500

    Set up playing functionality with keyboard events in addition to existing click events

--- scripts/play_keyboard.js
+++ scripts/play_keyboard.js
@@ -1,11 +1,35 @@
-let id_list = ['c-natural-1', 'c-sharp', 'd-natural', 'd-sharp', 'e-natural', 'f-natural', 'f-sharp', 'g-natural', 'g-sharp', 'a-natural', 'a-sharp', 'b-natural', 'c-natural-2'];
+const id_list = ['c-natural-1', 'c-sharp', 'd-natural', 'd-sharp', 'e-natural', 'f-natural', 'f-sharp', 'g-natural', 'g-sharp', 'a-natural', 'a-sharp', 'b-natural', 'c-natural-2'];
+
+const key_list = ['a', 'w', 's', 'e', 'd', 'f', 't', 'g', 'y', 'h', 'u', 'j', 'k'];
+
+const key_dict = {}
 
 
-function play_note(event) {
-    document.getElementById(`${event.target.id}-audio`).load();
-    document.getElementById(`${event.target.id}-audio`).play();
+
+for (let i = 0; i < key_list.length; i++) {
+    key_dict[key_list[i]] = id_list[i] + '-audio';
+}
+
+function play_note(id) {
+    document.getElementById(id).load();
+    document.getElementById(id).play();
+}
+
+function click_key(event) {
+    let id = `${event.target.id}-audio`;
+    play_note(id);
+}
+
+function press_key(KeyBoardEvent) {
+    let id = key_dict[KeyBoardEvent.key];
+    if (id){
+        play_note(id);
+    }
+    
 }
 
 for (let el of id_list) {
-    document.getElementById(el).addEventListener('mousedown', play_note);
+    document.getElementById(el).addEventListener('mousedown', click_key);
+    document.addEventListener('keypress', press_key);
 }
+
