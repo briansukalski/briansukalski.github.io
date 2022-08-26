1aefe972e54d1e91e902444d082b804de1af2f12
briansukalski <sukalskibrian@gmail.com>
2022-08-25 22:12:40 -0500

    Set up score counter for game

--- scripts/2048.js
+++ scripts/2048.js
@@ -1,8 +1,13 @@
-const gridDict = {1: "11", 2: "12", 3: "13", 4: "14", 5: "21", 6: "22", 7: "23", 8: "24", 9: "31", 10: "32", 11: "33", 12: "34", 13: "41", 14: "42", 15: "43", 16: "44"};
+const gridDict = {1: '11', 2: '12', 3: '13', 4: '14', 5: '21', 6: '22', 7: '23', 8: '24', 9: '31', 10: '32', 11: '33', 12: '34', 13: '41', 14: '42', 15: '43', 16: '44'};
+
+let score = 0;
+document.getElementById('score').innerHTML = score;
 
 function pressKey() {
     let newLocation = Math.ceil(Math.random() * 16);
-    document.getElementById(gridDict[newLocation]).innerHTML = "2";
+    document.getElementById(gridDict[newLocation]).innerHTML = '2';
+    score += 2;
+    document.getElementById('score').innerHTML = score;
 }
 
 document.addEventListener('keypress', pressKey);
