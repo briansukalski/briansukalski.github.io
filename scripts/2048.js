a4e9a1fd1242afef426f8dd099ac6691a7866f2a
briansukalski <sukalskibrian@gmail.com>
2022-08-30 21:08:36 -0500

    Set up keyboard listener to accept only wasd keys, upper or lowercase

--- scripts/2048.js
+++ scripts/2048.js
@@ -1,20 +1,25 @@
 const gridDict = {1: '11', 2: '12', 3: '13', 4: '14', 5: '21', 6: '22', 7: '23', 8: '24', 9: '31', 10: '32', 11: '33', 12: '34', 13: '41', 14: '42', 15: '43', 16: '44'};
 
+const acceptedInput = ['a', 'A', 's', 'S', 'd', 'D', 'w', 'W'];
+
 let score = 0;
 document.getElementById('score').innerHTML = score;
 
-function pressKey() {
-    let newTileScore;
-    let newLocation = Math.ceil(Math.random() * 16);
-    let randNum = Math.random();
-    if (randNum < .75) {
-        newTileScore = 2;
-    } else {
-        newTileScore = 4;
-    }
-    document.getElementById(gridDict[newLocation]).innerHTML = newTileScore;
-    score += newTileScore;
-    document.getElementById('score').innerHTML = score;
+function pressKey(KeyBoardEvent) {
+    let id = KeyBoardEvent.key;
+        if (acceptedInput.includes(id)) {
+            let newTileScore;
+            let newLocation = Math.ceil(Math.random() * 16);
+            let randNum = Math.random();
+            if (randNum < .75) {
+                newTileScore = 2;
+            } else {
+                newTileScore = 4;
+            }
+            document.getElementById(gridDict[newLocation]).innerHTML = newTileScore;
+            score += newTileScore;
+            document.getElementById('score').innerHTML = score;
+        }
 }
 
 function startGame() {
